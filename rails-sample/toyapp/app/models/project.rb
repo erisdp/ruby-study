ab0540a02667f2dd8435e1a8bda7b2877b91d596
eris-dwarf-planet <bo.od.com001@gmail.com>
2016-04-19 16:15:25 +0900

    toyapp - delete file.

--- rails-sample/toyapp/app/models/project.rb
+++
@@ -1,8 +0,0 @@
-class Project < ActiveRecord::Base
-    has_many :tasks;
-    # validates :title, presence: true;
-    validates :title,
-        presence: { message: "入力してください" },
-        length: { minimum: 3, message: "短すぎます" };
-
-end
--- rails-sample/toyapp/config/routes.rb
+++ rails-sample/toyapp/config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  #resources :projects do
+  #  resources :tasks, only: [:create, :destroy]
+  #end
 
-  resources :projects do
-    resources :tasks, only: [:create, :destroy]
-  end
-
-  root 'projects#index'
-  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+  #root 'projects#index'
+  #post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
 
   # static
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
- 
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
